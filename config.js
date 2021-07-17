461e6e9217a47e5ab9c599849de5d2ed0c272132
linhtrungne <87581036+linhtrungne@users.noreply.github.com>
2021-07-18 01:16:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Cậu à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao cậu cứ đi nhầm đường rồi lại
-    Lạc vào tim tớ thế kia`,
+    mà sao cậu cứ đi nhầm đường rồi lại
+    lạc vào tim tớ thế kia`,
     btnIntro: 'ấn zô đây ne',
     title: 'Hình như tui đã iu cậu ngay từ cái nhìn đầu tiên ><',
     desc: 'Phải chăng tui đã say ngay từ lúc thấy nụ cười ấy 😆',
     btnYes: 'Tui cứ thích cậu đấy ❤',
     btnNo: 'Cậu đang mơ đấy à :3',
-    question:'Trên thế giới có gần 8 tỉ người mà sao tớ lại yêu cậu ❤',
+    question:'Trên thế giới có gần 8 tỉ người mà sao cậu lại iu tớ ❤',
     btnReply: 'Gửi cho tớ 😆',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Iu thì iu mà không iu thì cũng phải iu ><',
     mess: 'Tớ biết mà 🥰. Iu cậu nhiều nhiều 👉👈',
     messDesc: 'Hôm nay 7h tớ qua đón cậu nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
