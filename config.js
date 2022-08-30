9723ffacd60b4ac8f9ab9575fe90b3dceb2d8226
Trung Linh Nguyen <87581036+linhtrungne@users.noreply.github.com>
2022-08-30 23:58:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Cậu à!',
+    introTitle: 'Tường Zi à!',
     introDesc: `Trái đất vốn lạ thường
-    mà sao cậu cứ đi nhầm đường rồi lại
-    lạc vào tim tớ thế kia`,
-    btnIntro: 'ấn zô đây ne',
-    title: 'Hình như tui đã iu cậu ngay từ cái nhìn đầu tiên ><',
-    desc: 'Phải chăng tui đã say ngay từ lúc thấy nụ cười ấy 😆',
-    btnYes: 'Tui cứ thích cậu đấy ❤',
-    btnNo: 'Cậu đang mơ đấy à :3',
-    question:'Trên thế giới có gần 8 tỉ người mà sao cậu lại iu tớ ❤',
+    mà sao em cứ đi nhầm đường rồi lại
+    lạc vào tim anh thế kia`,
+    btnIntro: 'ấn zô đây để zô tim anh',
+    title: 'Hình như anh đã iu em ngay từ đầu nhìn thấy em ><',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy 😆',
+    btnYes: 'Em cứ iu anh đấy ❤',
+    btnNo: 'Anh đang mơ đấy à :3',
+    question:'Trên thế giới có gần 8 tỉ người mà sao em lại iu anhhh ❤',
     btnReply: 'Gửi cho tớ 😆',
     reply: 'Iu thì iu mà không iu thì cũng phải iu ><',
-    mess: 'Tớ biết mà 🥰. Iu cậu nhiều nhiều 👉👈',
+    mess: 'Anh biết mò 🥰. Iu em nhìu lắm lám lun 👉👈',
     messDesc: 'Mai 9g tớ qua đón cậu đi cháy phố nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/Trung.Linh.Nguyenn'
