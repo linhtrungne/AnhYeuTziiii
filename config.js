5a5ac13f9f68a802ab5c49773e963434c161e0e8
Trung Linh Nguyen <87581036+linhtrungne@users.noreply.github.com>
2022-08-31 00:04:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,6 +13,6 @@ const CONFIG = {
     reply: 'Iu thì iu mà không iu thì cũng phải iu ><',
     mess: 'Anh biết mò 🥰. Iu em nhìu lắm lám lun 👉👈',
     messDesc: 'Mai 9g tớ qua đón cậu đi cháy phố nhé công chúa.',
-    btnAccept: 'Okiiiii lunnnn <3',
+    btnAccept: 'Okiiiii lunnnnn <3',
     messLink: 'https://m.me/Trung.Linh.Nguyenn'
 }
