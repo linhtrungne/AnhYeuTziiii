97150f4d99613e694d44636b5f6fe775e865b721
Trung Linh Nguyen <87581036+linhtrungne@users.noreply.github.com>
2022-08-31 09:41:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ const CONFIG = {
     btnYes: 'Em cứ iu anh đấy ❤',
     btnNo: 'Anh đang mơ đấy à :3',
     question:'Trên thế giới có gần 8 tỉ người mà sao em lại iu anhhh ❤',
-    btnReply: 'Gửi cho tớ 😆',
+    btnReply: 'Gửi cho anh 😆',
     reply: 'Em cũng iu anh siu nhìu nhìu lun ><',
-    mess: 'Anh biết mò 🥰. Iu em nhìu lắm lám lun 👉👈',
-    messDesc: 'Mai 9g anh qua đón em đi chơi nhé công chúa.',
+    mess: 'Anh biết mò 🥰. Iu em nhìu lắm lắm lun 👉👈',
+    messDesc: 'Tối 7g anh qua đón em đi chơi nhé công chúa.',
     btnAccept: 'Okiii lun <3',
     messLink: 'https://m.me/Trung.Linh.Nguyenn'
 }
