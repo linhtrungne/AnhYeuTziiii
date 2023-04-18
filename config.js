df16f2b1bcf939626113bc0f7f501f4d0ce688e3
Trung Linh Nguyen <87581036+linhtrungne@users.noreply.github.com>
2023-04-18 23:45:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Thanh Phương àaa!',
+    introTitle: 'Tường Vi àaaaaa!',
     introDesc: `Trái đất vốn lạ thường
     mà sao em cứ đi nhầm đường rồi lại
     lạc vào tim anh thế kia`,
@@ -8,11 +8,11 @@ const CONFIG = {
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy 😆',
     btnYes: 'Em cứ iu anh đấy ❤',
     btnNo: 'Anh đang mơ đấy à :3',
-    question:'Hình như anh lỡ yêu em nhìu lắm rui Thanh Phương àaaa ❤',
+    question:'Hình như anh lỡ yêu em nhìu lắm rui Tzi àaaa ❤',
     btnReply: 'Gửi cho anh ne 😆',
     reply: 'Em cũng iu anh siu nhìu nhìu lun ><',
     mess: 'Anh biết mò 🥰. Anh iu em 🫶',
     messDesc: 'Tối mai 7g anh qua đón em đi chơi nhé công chúa.',
     btnAccept: 'Okiii lun <3',
-    messLink: 'https://m.me/TrungLinhNguyennn'
+    messLink: 'https://m.me/TrungLinhNguyenn'
 }
